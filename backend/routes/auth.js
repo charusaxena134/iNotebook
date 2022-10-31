b387974384def5837b2e24931307b4dcdc806fea
Charu Saxena <charusaxena134@gmail.com>
2022-10-31 12:20:23 +0530

    created a update and delete api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
+const fetchuser = require("../middleware/fetchuser");
 // create a user using POST "/api/auth/" Doesn't required auth
 const JWT_SECRET = "Charusaxena";
 router.post(
@@ -56,4 +57,57 @@ router.post(
     }
   }
 );
+
+// Authenticate a user using post "/api/auth/login" No login required
+router.post(
+  "/login",
+  [
+    body("email", "enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    // if there are bad errors, return bad requuest and the error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: "Please truy to login with correct credentials" });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        res
+          .status(400)
+          .json({ errors: "Please try to login with correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json(authToken);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("Internal server error");
+    }
+  }
+);
+
+// Route 3: Get logged user details using: Post "api/auth/getuser". Login required
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("password");
+    res.send(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal server error");
+  }
+});
 module.exports = router;
